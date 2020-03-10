c3afd2c391331aed9e22f75ca8a4405b78e9dcfb
PaauRF <paaurodriiguez@gmail.com>
2020-03-10 15:43:51 -0600

    prueba

--- contactos.php
+++ contactos.php
@@ -8,8 +8,8 @@
     <!-- <link rel="stylesheet" href="dist/css/base.min.css"> -->
     <script src="plugins/datatables/jquery.dataTables.min.js"></script>
     <script src="plugins/datatables/dataTables.bootstrap.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>   
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>    -->
     <div class="app-main__outer">
         <div class="app-main__inner">
             <div class="app-page-title">
