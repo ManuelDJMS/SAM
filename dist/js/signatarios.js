a1d1d738af0535fcb6ac681dbd650acbf8e584ab
PaauRF <paaurodriiguez@gmail.com>
2020-03-20 18:03:16 -0600

    Agregar signatarios
    90%

---
+++ dist/js/signatarios.js
@@ -0,0 +1,224 @@
+$(document).ready(function(){
+  $('.btnEditarG').hide();
+  $('#NombreE').hide();
+    obtener_signatarios();
+    obtener_personal();
+    obtener_laboratorio();
+
+    // ============= EVENTO DE EL BOTON GUARDAR (MANDAR POST)=================
+	$('html').on('click', '.btnGuardar', function(){
+    //============= EN ESTE METODO SE CREA UN OBJETO CON TODOS LOS DATOS DEL FORMULARIO =======================================  
+     var obj = new Object();
+      obj.Nombre = $.trim($('#Nombre').val());
+       obj.Laboratorio = $.trim($('#Lab').val());
+       obj.accion = $(this).attr("id").split('_')[1];
+       obj.idUsuario = $(this).attr("id").split('_')[2];
+       // ============= SE VALIDA SI CIERTOS CAMPOS ESTAN LLENOS =======================
+       if(obj.Nombre != '')
+       {
+          if(Lab != '')
+              {
+                registrar_signatario(obj);
+              }
+       
+              else
+              {
+                alerta_error("INFORMACIÓN FALTANTE","Debe seleccionar un laboratorio");
+              }
+          }
+          else
+          {
+            alerta_error("INFORMACIÓN FALTANTE","Debe seleccionar un usuario");
+          }
+    });
+  // =======================================================================
+    // =========== EVENTO DE EDITAR EN LA TABLA ==============================
+    $('html').on('click', '.btnEditar', function(){
+      //SE SIMULA EL CLICK DEL TAB 
+      $("#tab-0").get(0).click();
+      //==================== SE MUESTRAN Y OCULTAN CIERTOS BOTONES =============================
+      $('.btnEditarG').show();
+      $('.btnGuardar').hide();
+      $('#Nombre').hide();
+      $('#NombreE').show();
+    
+      //=======================================================================================
+      // ================ SE ASIGNA ID A EDITAR ===============================================
+      var id = $(this).attr('id').split('_')[1];
+      $('.btnEditarG').attr('id',$(this).attr('id'));
+      obtener_signatario(id);
+  });
+  //========================================================================
+  //============== EVENTO DEL BOTON DE EDITAR Y GUARDAR =====================
+  $('html').on('click', '.btnEditarG', function(){
+    //============= EN ESTE METODO SE CREA UN OBJETO CON TODOS LOS DATOS DEL FORMULARIO =======================================  
+    var obj = new Object();
+    obj.Nombre = $.trim($('#NombreE').val());
+    obj.Laboratorio = $.trim($('#Lab').val());
+    obj.accion = $(this).attr("id").split('_')[0];
+    obj.idUsuario = $(this).attr("id").split('_')[1];
+       if(Lab != '')
+           {
+             registrar_signatario(obj);
+           }
+    
+           else
+           {
+             alerta_error("INFORMACIÓN FALTANTE","Debe seleccionar un laboratorio");
+           }
+  });
+});
+//----------CARGAR Y LIMPIAR LA TABLA DE USUARIO/SIGNATARIOS------------------
+    function obtener_signatarios(){
+    var opc = "obtener_signatarios";
+    $('.line-scale-pulse-out').show();
+    regenerar_tablaSignatarios();
+    $.post("dist/fw/signatarios.php",{opc:opc},function(data){
+        if(data){
+            var html = '';
+            for (var i = 0; i < data.length; i++){
+              html += '<tr class="edita_error" id="error_' + $.trim(data[i].idUsuario) + '">';
+              html += '<td>' + $.trim(data[i].Nombre) +  '</td>';
+              html += '<td>' + $.trim(data[i].Email) + '</td>';
+              html += '<td>' + $.trim(data[i].UnidadNegocio) + '</td>';
+              html += '<td>' + $.trim(data[i].Laboratorio) + '</td>';
+              html += '<td class="btnEditar" id="edit_'+data[i].idUsuario+'"><span class="font-icon-wrapper lnr-pencil" ></span></td>';
+              html += '</tr>';          
+              $('#NombreUser').text(data.Nombre);                
+            }
+            $('#example3 tbody').html(html);
+            $('#example3').DataTable({
+                "paging": true,
+                "lengthChange": true,
+                "searching": true,
+                "ordering": true,
+                "info": true,
+                "autoWidth": true
+            });
+        }
+        $('.line-scale-pulse-out').hide();
+    },'json');
+} 
+
+function regenerar_tablaSignatarios(){
+  $('#div_registros1').html("");
+  var html = "";
+  html += '<table id="example3" class="table table-bordered table-striped dataTable">';
+  html += '<thead>';
+  html += '<tr>';
+  html += '<th>Nombre</th>';
+  html += '<th>Email</th>';
+  html += '<th>Unidad Negocio</th>';
+  html += '<th>Laboratorio</th>';
+  html += '<th>Editar</th>';
+  html += '</tr>';
+  html += '</thead>';
+  html += '<tbody>';
+  html += '</tbody>';
+  html += '</table>';
+  $('#div_registros1').html(html);
+}
+//---------------------------------------------------------
+
+// CARGAR COMBO DE NOMBRE DE USUARIOS-----------------------
+function obtener_personal(){
+  var opc = "obtener_personal";
+  $('#idUsuario').html('');
+  $.post("dist/fw/signatarios.php",{'opc':opc},function(data){
+      if(data){
+         $('#Nombre').html(data);
+      }
+  },'json');
+}
+
+//---------------------------------------------------------
+// CARGAR COMBO DE LABORATORIO-----------------------
+function obtener_laboratorio(){
+  var opc = "obtener_laboratorio";
+  $('#idUnidadNegocio').html('');
+  $.post("dist/fw/signatarios.php",{'opc':opc},function(data){
+      if(data){
+         $('#Lab').html(data);
+      }
+  },'json');
+}
+//---------------------------------------------------------
+
+
+// CARGAR COMBO DE LABORATORIO-----------------------
+function registrar_signatario(obj){
+  alert(obj.Nombre);
+  alert(obj.Laboratorio);
+  alert(obj.accion);
+  var opc = "registrar_signatario";
+  $.post("dist/fw/signatarios.php",{'opc':opc, 'obj':JSON.stringify(obj)},function(data){
+		if(data){
+      alerta("¡Guardado!", "Signatario registrado, ¿desea seguir en agregando signatarios", "success");
+      obtener_signatarios();
+    }else{
+      alerta_error("¡Error!","Error al guardar los datos");
+  }
+  }, 'json');
+}
+
+//---------------------------------------------------------
+
+
+function obtener_signatario(id){
+
+    var opc = "obtener_signatario";
+    $.post("dist/fw/signatarios.php",{'opc':opc, 'id':id},function(data){
+        if(data){
+          $('#NombreE').val(data.idUsuario);
+          $('#Lab').val(data.idUnidadNegocio);
+        }
+        else
+        {
+          alerta_error("Error", "Error al recibir los datos");
+        }
+    },'json');
+}
+
+function limpia_formulario(){
+  $("#Nombre").val("");
+  $("#Lab").val("");
+} 
+
+
+      function alerta(titulo, mensaje, icono){
+      const swalWithBootstrapButtons = Swal.mixin({
+          customClass: {
+            confirmButton: 'btn btn-success',
+            cancelButton: 'btn btn-danger'
+          },
+          buttonsStyling: false
+        })
+        
+        swalWithBootstrapButtons.fire({
+          title: titulo,
+          text: mensaje,
+          icon: icono,
+          showCancelButton: true,
+          confirmButtonText: 'Aceptar',
+          cancelButtonText: 'Salir',
+          reverseButtons: true
+        }).then((result) => {
+          if (result.value) {
+            location.reload();
+          } else if (
+            result.dismiss === Swal.DismissReason.cancel
+          ) {
+            window.location="http://localhost:8080/dashboard/SAM/index.php";
+          }
+        })
+    }
+    
+    function alerta_error(titulo, texto){
+      Swal.fire({
+        icon: 'error',
+        title: titulo,
+        text: texto
+        // footer: '<a href>Why do I have this issue?</a>'
+      })
+    }
+  
---
+++ signatarios.php
@@ -0,0 +1,91 @@
+<?php
+     include_once("header.php");
+     include_once("validates.php");
+     include_once("banner.php");
+?>
+<!-- SE PONE EN COMENTARIOS PARA UN MEJOR DISEÑO -->
+<link rel="stylesheet" href="dist/css/base.css">
+<script src="plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="plugins/datatables/dataTables.bootstrap.min.js"></script> 
+
+<div class="app-main__outer">
+    <div class="app-main__inner">
+        <div class="app-page-title">
+            <div class="page-title-wrapper">
+                <div class="page-title-heading">
+                    <div class="page-title-icon">
+                        <i class="lnr-bold icon-gradient bg-love-kiss">
+                        </i>
+                    </div>
+                    <div>Signatarios de Laboratorio
+                        <div class="page-title-subheading">Administra los signatarios asignados a cada laboratorio y unidad de negocio.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>            
+        <ul class="body-tabs body-tabs-layout tabs-animated body-tabs-animated nav">
+            <li class="nav-item">
+                <a role="tab" class="nav-link active" id="tab-0" data-toggle="tab" href="#tab-content-0">
+                    <span>Agregar</span>
+                </a>
+            </li>
+         </ul>
+          <!-- ===================================AQUI EMPIEZA EL CODIGO PARA AGREGAR SIGNATARIO====================================================================== -->
+        <div class="tab-content">
+            <div class="tab-pane tabs-animation fade show active" id="tab-content-0" role="tabpanel">
+                <div class="main-card mb-3 card">
+                    <div class="card-body">
+                        <form id="signupForm">
+                            <div class="row">
+                                <div class="col-md-7">
+                                    <div class="main-card mb-3 card">
+                                        <div class="card-body"><h5 class="card-title">LISTA DE SIGNATARIOS</h5>
+                                            <div id="div_registros1" class="box-body">
+                                                <table id="example3" class="table table-bordered table-striped dataTable">
+                                                    <thead>
+                                                        <tr>
+                                                            <th>Nombre</th>
+                                                            <th>Email</th>
+                                                            <th>Unidad Negocio</th>
+                                                            <th>Laboratorio</th>
+                                                            <th>Editar</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                    </tbody>
+                                                </table>
+                                            </div><!-- /.box-body -->
+                                        </div>  
+                                    </div>
+                                </div>
+                                <div class="col-md-5">
+                                    <div class="main-card mb-3 card">
+                                        <div class="card-body"><h5 class="card-title" >AGREGAR NUEVO SIGNATARIO</h5>  
+                                            <label for="Nombre" class="col-sm-4 control-label">Empleado</label>
+                                                <div class="col-sm-8">
+                                                    <select type="select" id="Nombre" name="Nombre" class="custom-select"></select>
+                                                </div>
+                                                <div class="col-sm-6">
+                                                    <div class="position-relative form-group">
+                                                        <input type="text" class="form-control" id="NombreE" name="NombreE" placeholder="" readonly="readonly"/>
+                                                    </div>
+                                                </div>    
+                                            <label for="Lab" class="col-sm-4 control-label">Laboratorio</label>
+                                                <div class="col-sm-6">
+                                                    <select type="select" id="Lab" name="Lab" class="custom-select"></select>
+                                                </div>
+                                        </div>  
+                                    </div>
+                                    <button type="button" class="mt-2 btn btn-primary float-right btnGuardar" id="btn_nuevo_0">Guardar</button>
+                                    <button type="button" class="mt-2 btn btn-primary float-right btnEditarG">Editar</button>   
+                                </div>
+                            </div>        
+                        </form>
+                    </div>
+                </div>
+            </div>
+        <script src="dist/js/signatarios.js"></script>
+        <?php include_once("footer.php");?>
+    </div>
+</div>    
